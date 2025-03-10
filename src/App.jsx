b7624abd0f9841ae63ff8acb98fbc7c7b2153b79
Jebakumar <jebakumar.6208@gmail.com>
2025-03-10 11:51:55 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React from 'react';
+import BookCard from "./components/bookcard";
 
 function App() {
   const books = [
@@ -34,7 +35,26 @@ function App() {
     },
   ];
 
-  return <>{/* write code to display all the books using */}</>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      }}
+    >
+      {books.map((book) => (
+        <BookCard
+          key={book.id}
+          name={book.name}
+          image={book.image}
+          genre={book.genre}
+          author={book.author}
+        />
+      ))}
+    </div>
+
+  );
 }
 
 export default App;
--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,16 @@
 // write the book component code here
+import React from "react";
+
+function BookCard({ name, image, genre, author }) {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "10px", margin: "10px", borderRadius: "5px", textAlign: "center", width: "200px" }}>
+      <img src={image} alt={name} style={{ width: "100px", height: "150px", marginBottom: "10px" }} />
+      <h3>{name}</h3>
+      <p><strong>Genre:</strong> {genre}</p>
+      <p><strong>Author:</strong> {author}</p>
+    </div>
+  );
+}
+
+export default BookCard;
+
